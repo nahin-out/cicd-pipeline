e60f993dd18f60aeb03fef6f9f92b698eff7e351
nahin-out <has9.nahin@gmail.com>
2026-03-15 12:32:33 +0600

    test devsecops pipeline

--- src/main/java/com/learning/pipeline/HelloController.java
+++ src/main/java/com/learning/pipeline/HelloController.java
@@ -12,6 +12,6 @@ public class HelloController {
 
         @GetMapping("/healthz")
         public String healthz() {
-                return "Healthy! and Asking for ci-cd confirmation if it is working bro";
+                return "Healthy! DevSecOps pipeline is working fine.";
         }
 }
